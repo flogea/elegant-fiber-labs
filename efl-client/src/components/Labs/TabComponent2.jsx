1e8f29ae78865017b9b2d822505495dd18031239
flogea <katua.logvinova@gmail.com>
2022-11-02 19:10:53 +0300

    small changes

--- efl-client/src/components/Labs/TabComponent2.jsx
+++ efl-client/src/components/Labs/TabComponent2.jsx
@@ -4,9 +4,24 @@ import { Context } from '../../Context';
 
 function TabComponent({ array }) {
   const { table2, setTable2 } = React.useContext(Context);
+  const [isEmpty, setEmpty] = React.useState(false);
 
   const handleChangeTable2 = (event) => {
-    setTable2({ ...table2, [event.target.name]: event.target.value });
+    if (!event.target.value === '') {
+      setEmpty(false);
+      setTable2({ ...table2, [event.target.name]: event.target.value });
+    } else {
+      setEmpty(true);
+    }
+  };
+
+  const handleBlur = (event) => {
+    console.log(event.target.value);
+    if (!(event.target.value === '')) {
+      setEmpty(false);
+    } else {
+      setEmpty(true);
+    }
   };
 
   return (
@@ -16,14 +31,16 @@ function TabComponent({ array }) {
           <div key={`${name}_${index}`} className="col s12 m6 l6">
             <label>{name}</label>
             <input
+              style={isEmpty ? { borderColor: 'red' } : { borderColor: 'green' }}
               key={`${name}_${index}`}
               type="number"
               step="any"
               name={name}
               className="input-table"
               onChange={handleChangeTable2}
+              onBlur={handleBlur}
               required="required"
-              inputmode="numeric"
+              inputMode="numeric"
               pattern="[1-9]"
             />
           </div>
