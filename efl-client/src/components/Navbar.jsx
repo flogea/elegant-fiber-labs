9eb0f0ad8478390cf93c1c317d04bbc454099c7c
flogea <katua.logvinova@gmail.com>
2022-11-02 19:10:28 +0300

    added navbar

--- efl-client/src/components/Navbar.jsx
+++ efl-client/src/components/Navbar.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
+import React from 'react';
+
+import '../styles/Home.scss';
+
+import logo from '../images/logo.svg';
 
 function Navbar() {
   return (
-    <div>
-        <nav>
-            <div className="nav-wrapper container">
-                <a href="#!" className="brand-logo">Logo</a>
-                <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-                <ul className="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">Javascript</a></li>
-                    <li><a href="mobile.html">Mobile</a></li>
-                </ul>
-            </div>
-        </nav>
-        <ul className="sidenav" id="mobile-demo">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
-        </ul>
-  </div>
-  )
+    <>
+      <nav className="container">
+        <div className="logo">
+          <a href="/" className="brand-logo">
+            <img src={logo} alt="Logo" />
+          </a>
+          {/* <a href="/" data-target="mobile-demo" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a> */}
+        </div>
+        <div className="navlist">
+          <ul className="">
+            <li>
+              <a href="/">Главная</a>
+            </li>
+            <li>
+              <a href="/">Лабы</a>
+            </li>
+            <li>
+              <a href="/">Контакты</a>
+            </li>
+            <li>
+              <a href="/">Эбаут</a>
+            </li>
+          </ul>{' '}
+        </div>
+      </nav>
+      {/* <ul className="sidenav" id="mobile-demo">
+        <li>
+          <a href="sass.html">Sass</a>
+        </li>
+        <li>
+          <a href="badges.html">Components</a>
+        </li>
+        <li>
+          <a href="collapsible.html">Javascript</a>
+        </li>
+        <li>
+          <a href="mobile.html">Mobile</a>
+        </li>
+      </ul> */}
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
