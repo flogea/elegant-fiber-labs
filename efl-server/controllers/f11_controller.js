d01871b6d1bcff9bc476c6d0ddbd8cfc623c19fa
flogea <katua.logvinova@gmail.com>
2022-11-17 23:46:23 +0300

    adapted for linux

--- efl-server/controllers/f11_controller.js
+++ efl-server/controllers/f11_controller.js
@@ -31,6 +31,7 @@ class f11_controller {
         photo,
       } = req.body;
       const photoName = res.locals.photoName;
+      console.log(req.body);
 
       const f11 = new F11model({
         data: table1,
@@ -112,7 +113,7 @@ class f11_controller {
           str,
       ];
 
-      fs.writeFile(`${id_lab}.txt`, `${data}`, (err) => {
+      fs.writeFile(`${id_lab}.txt`, data, (err) => {
         if (err) throw err;
       });
 
