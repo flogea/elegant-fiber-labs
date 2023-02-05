5383f7d9d8f06874225d3b10183bdc2bcbd76038
flogea <katua.logvinova@gmail.com>
2023-02-05 23:50:41 +0300

    add indexes

--- efl-client/src/components/Labs/F12Research1.jsx
+++ efl-client/src/components/Labs/F12Research1.jsx
@@ -5,12 +5,11 @@ import { Context } from '../../Context';
 import difr from '../../images/diffraction.png';
 
 function F12Research1() {
-  const FColName1 = 'Pсигн, В';
-  const SColName1 = 'I1, мкА';
+  const FColName1 = 'P<sub>сигн</sub>, В';
+  const SColName = 'I<sub>1</sub>, мкА';
   const array1 = [0.25, 0.5, 0.75, 2, 'Зашкаливает'];
 
   const FColName2 = 'a, мм';
-  const SColName2 = 'I1, мкА';
   const array2 = [1, 2, 3, 4, 5, 6, 7, 8];
 
   const { table12_1, setTable12_1, table1, table2, table3 } = React.useContext(Context);
@@ -26,8 +25,17 @@ function F12Research1() {
         <div className="main-text">6 Зафиксируйте частоту работы генератора</div>
         <div className="row">
           <div className="col">
-            <label>Частота, МГц</label>
-            <input type="text" className="input-table" name="1" onChange={handleChangeTable12_1} />
+            <div className="input col s12 m6 l6">
+              <input
+                type="text"
+                name="1"
+                className="validate"
+                required="required"
+                onChange={handleChangeTable12_1}
+              />
+              <span htmlFor="1">Частота, МГц</span>
+              <i></i>
+            </div>
           </div>
         </div>
         <h2>Углы дифракции</h2>
@@ -120,7 +128,7 @@ function F12Research1() {
           следует снимать с индикатора над поворотными ручками. Зашкаливает - крайние правые
           положения ручек.
         </div>
-        <F11tab FColName={FColName1} SColName={SColName1} array={array1} research="1" />
+        <F11tab FColName={FColName1} SColName={SColName} array={array1} research="1" />
       </div>
 
       <div>
@@ -138,32 +146,40 @@ function F12Research1() {
           плоскости лазера появится отраженный луч. Вращайте ручку таким образом, чтобы отраженный
           луч находился слева от отверстия излучателя.
         </div>
-        <F11tab FColName={FColName2} SColName={SColName2} array={array2} research="2" />
-        <div className="main-text">13 Зафиксируйте значение a, при котором наибольший I1.</div>
+        <F11tab FColName={FColName2} SColName={SColName} array={array2} research="2" />
+        <div className="main-text">
+          13 Зафиксируйте значение a, при котором наибольший I<sub>1</sub>.
+        </div>
         <div className="row">
           <div className="col">
-            <label>a оптимальное, мм</label>
-            <input
-              type="text"
-              className="input-table"
-              name="aopt"
-              onChange={handleChangeTable12_1}
-            />
+            <div className="input col s12 m6 l6">
+              <input
+                type="text"
+                name="aopt"
+                className="validate"
+                required="required"
+                onChange={handleChangeTable12_1}
+              />
+              <span htmlFor="aopt">
+                a<sub>оптимальное</sub>, мм
+              </span>
+              <i></i>
+            </div>
           </div>
         </div>
       </div>
       <div>
         <h2>Зависимость дифракционной эффективности от мощности при оптимальном угле падения</h2>
         <div className="main-text">
-          14 Установите a, равное a оптимальное вращением нижнего винта держателя, на котором он
-          закреплен.
+          14 Установите a, равное a<sub>оптимальное</sub> вращением нижнего винта держателя, на
+          котором он закреплен.
         </div>
         <div className="main-text">
           15 Изменяя мощность сигнала на генераторе путем вращения поворотных ручек в нижней части
           лицевой панели генератора, зафиксируйте зависимость тока на фотоприемнике. Мощность
           следует снимать с индикатора над поворотными ручками.
         </div>
-        <F11tab FColName={FColName1} SColName={SColName1} array={array1} research="3" />
+        <F11tab FColName={FColName1} SColName={SColName} array={array1} research="3" />
       </div>
     </>
   );
--- efl-client/src/components/Labs/F12Research2.jsx
+++ efl-client/src/components/Labs/F12Research2.jsx
@@ -5,12 +5,11 @@ import { Context } from '../../Context';
 import difr from '../../images/diffraction.png';
 
 function F12Research2() {
-  const FColName1 = 'Pсигн, В';
-  const SColName1 = 'I1, мкА';
+  const FColName1 = 'P<sub>сигн</sub>, В';
+  const SColName = 'I<sub>1</sub>, мкА';
   const array1 = [0.25, 0.5, 0.75, 2, 'Зашкаливает'];
 
   const FColName2 = 'a, мм';
-  const SColName2 = 'I1, мкА';
   const array2 = [1, 2, 3, 4, 5, 6, 7, 8];
 
   const { table12_2, setTable12_2, table1, table2, table3 } = React.useContext(Context);
@@ -29,8 +28,17 @@ function F12Research2() {
         </div>
         <div className="row">
           <div className="col">
-            <label>Частота, МГц</label>
-            <input type="text" className="input-table" name="1" onChange={handleChangeTable12_2} />
+            <div className="input col s12 m6 l6">
+              <input
+                type="text"
+                name="1"
+                className="validate"
+                required="required"
+                onChange={handleChangeTable12_2}
+              />
+              <span htmlFor="1">Частота, МГц</span>
+              <i></i>
+            </div>
           </div>
         </div>
         <h2>Углы дифракции</h2>
@@ -122,7 +130,7 @@ function F12Research2() {
           лицевой панели генератора, зафиксируйте зависимость тока на фотоприемнике. Мощность
           следует снимать с индикатора над поворотными ручками.
         </div>
-        <F11tab FColName={FColName1} SColName={SColName1} array={array1} research="1" />
+        <F11tab FColName={FColName1} SColName={SColName} array={array1} research="1" />
       </div>
 
       <div>
@@ -140,32 +148,40 @@ function F12Research2() {
           плоскости лазера появится отраженный луч. Вращайте ручку таким образом, чтобы отраженный
           луч находился слева от отверстия излучателя
         </div>
-        <F11tab FColName={FColName2} SColName={SColName2} array={array2} research="2" />
-        <div className="main-text">23 Зафиксируйте значение a, при котором наибольший I1.</div>
+        <F11tab FColName={FColName2} SColName={SColName} array={array2} research="2" />
+        <div className="main-text">
+          23 Зафиксируйте значение a, при котором наибольший I<sub>1</sub>.
+        </div>
         <div className="row">
           <div className="col">
-            <label>a оптимальное, мм</label>
-            <input
-              type="text"
-              className="input-table"
-              name="aopt"
-              onChange={handleChangeTable12_2}
-            />
+            <div className="input col s12 m6 l6">
+              <input
+                type="text"
+                name="aopt"
+                className="validate"
+                required="required"
+                onChange={handleChangeTable12_2}
+              />
+              <span htmlFor="aopt">
+                a<sub>оптимальное</sub>, мм
+              </span>
+              <i></i>
+            </div>
           </div>
         </div>
       </div>
       <div>
         <h2>Зависимость дифракционной эффективности от мощности при оптимальном угле падения</h2>
         <div className="main-text">
-          24 Установите a, равное a оптимальное вращением нижнего винта держателя, на котором он
-          закреплен.
+          24 Установите a, равное a<sub>оптимальное</sub> вращением нижнего винта держателя, на
+          котором он закреплен.
         </div>
         <div className="main-text">
           25 Изменяя мощность сигнала на генераторе путем вращения поворотных ручек в нижней части
           лицевой панели генератора, зафиксируйте зависимость тока на фотоприемнике. Мощность
           следует снимать с индикатора над поворотными ручками.
         </div>
-        <F11tab FColName={FColName1} SColName={SColName1} array={array1} research="3" />
+        <F11tab FColName={FColName1} SColName={SColName} array={array1} research="3" />
       </div>
     </>
   );
