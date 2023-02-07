fa3370ec12d2a62c63b89dc1d1dae250cf1b8f72
flogea <katua.logvinova@gmail.com>
2023-02-08 02:02:21 +0300

    M11 lab & foldable component

--- efl-client/src/pages/Labs/index.js
+++ efl-client/src/pages/Labs/index.js
@@ -4,3 +4,5 @@ export { default as F13 } from './F13';
 export { default as F14 } from './F14';
 export { default as F15 } from './F15';
 export { default as F21 } from './F21';
+export { default as F22 } from './F22';
+export { default as M11 } from './M11';
