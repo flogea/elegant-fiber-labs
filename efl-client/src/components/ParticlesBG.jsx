6c0f41b0b9904eefc2b14285a1d9b8c615668198
flogea <katua.logvinova@gmail.com>
2023-03-08 23:21:05 +0300

    ParticleJS

---
+++ efl-client/src/components/ParticlesBG.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import Particles from 'react-tsparticles';
+import { useCallback } from 'react';
+import { loadFull } from 'tsparticles';
+
+function ParticlesBG() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <div style={{ position: 'absolute', zIndex: '-1000' }}>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: '#1c1c1e',
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              // onClick: {
+              //   enable: true,
+              //   mode: 'push',
+              // },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#fff',
+            },
+            links: {
+              color: '#fff',
+              distance: 150,
+              enable: true,
+              opacity: 0.2,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.3,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 1, max: 3 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+}
+
+export default ParticlesBG;
