b815659d84d4b24274416b8783230f9d0c80def9
flogea <katua.logvinova@gmail.com>
2023-04-06 23:04:22 +0300

    made a prelview for all type of files

--- efl-client/src/components/Labs/InputWithPreview.jsx
+++ efl-client/src/components/Labs/InputWithPreview.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setFileName } from '../../redux/slices/fileNameSlice';
+import { setFileURL } from '../../redux/slices/fileURLSlice';
 import Foldable from './Foldable';
 
 function InputWithPreview({ num, ext }) {
-  const [fileURL, setFileURL] = React.useState('');
   const fileName = useSelector((state) => state.fileNameSlice);
+  const fileURL = useSelector((state) => state.fileURLSlice);
+
   const dispatch = useDispatch();
 
   const nameOfFile = `file${num}`;
 
   const onChangeInput = (e) => {
-    const file = e.target.files[0];
+    let file = e.target.files[0];
     const currentName = file.name;
+    if (ext === 'v') {
+      file = new Blob([file], { type: 'text/plain' });
+    }
     dispatch(setFileName({ ...fileName, [e.target.name]: currentName }));
     fileReader.readAsDataURL(file);
   };
 
   const fileReader = new FileReader();
   fileReader.onloadend = () => {
-    setFileURL(fileReader.result);
+    dispatch(setFileURL({ [`${nameOfFile}URL`]: fileReader.result }));
   };
 
   return (
@@ -31,29 +36,29 @@ function InputWithPreview({ num, ext }) {
           name={nameOfFile}
           required="required"
           id={`upload_file_input${num}`}
-          accept={`.${ext}`}
+          accept={`text/plain, .${ext}`}
         />
         <label htmlFor={`upload_file_input${num}`}></label>
       </div>
       <span id={`output__data${num}`} className="output__span">
         {fileName[nameOfFile]}
       </span>
-      {fileURL && ext === 'png' ? (
+      {fileURL[`${nameOfFile}URL`] && ext === 'png' ? (
         <div className="foldable__content">
           <Foldable header="Предпросмотр">
             <div className="centeredInRow">
-              <img src={fileURL} />
+              <img src={fileURL[`${nameOfFile}URL`]} />
             </div>
           </Foldable>
         </div>
       ) : null}
-      {fileURL && ext === 'pdf' ? (
+      {fileURL[`${nameOfFile}URL`] && (ext === 'pdf' || ext === 'v') ? (
         <div className="foldable__content">
           <Foldable header="Предпросмотр">
             <div className="centeredInRow">
               <iframe
-                style={{ width: '100%', height: '80vh' }}
-                src={fileURL}
+                style={{ width: '100%', height: '50vh' }}
+                src={fileURL[`${nameOfFile}URL`]}
                 frameBorder="0"></iframe>
             </div>
           </Foldable>
